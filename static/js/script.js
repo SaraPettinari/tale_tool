ef3890081aa62fa63592563339d2e50674728192
SaraPettinari <sara.pettinari@unicam.it>
2023-09-13 14:51:57 +0200

    added new layouts

--- static/js/script.js
+++ static/js/script.js
@@ -118,6 +118,183 @@ function updateTree() {
     }
 }
 
+function testdagre(data) {
+    nodes = []
+    for (n in data.nodes) {
+        //node_data.push({ id: in_data.nodes[n]['Event_Id'], properties: in_data.nodes[n] })
+        node = data.nodes[n]
+        if (node.label != null) {
+            node.label = node.label.replaceAll("_", "\n")
+        }
+        nodes.push({ data: { id: node.id, label: node.label } })
+    }
+
+    edges = []
+    for (e in data.edges) {
+        edge = data.edges[e]
+        edge_label = edge.label
+        edges.push({ data: { source: edge.from, target: edge.to, id: e, label: edge_label } })
+    }
+
+    elements = nodes.concat(edges)
+    console.log(nodes)
+
+    var g = new dagre.graphlib.Graph().setGraph({});
+
+    for (n in nodes) {
+        label = nodes[n].data.id
+        g.setNode(label, { label: label, fill: "#afa" });
+    }
+    for (e in edges) {
+
+        g.setEdge(edges[e].data.source, edges[e].data.target, {});
+    }
+    // Create the renderer
+    var render = new dagreD3.render();
+
+    // Set up an SVG group so that we can translate the final graph.
+    var svg = d3.select('svg'),
+        svgGroup = svg.append('g');
+
+    d3.color("steelblue")
+
+    // Run the renderer. This is what draws the final graph.
+    render(svgGroup, g);
+
+}
+
+function generate_dagre(data) {
+    nodes = []
+    for (n in data.nodes) {
+        //node_data.push({ id: in_data.nodes[n]['Event_Id'], properties: in_data.nodes[n] })
+        node = data.nodes[n]
+        if (node.label != null) {
+            node.label = node.label.replaceAll("_", "\n")
+        }
+        nodes.push({ data: { id: node.id, label: node.label } })
+    }
+
+    edges = []
+    for (e in data.edges) {
+        edge = data.edges[e]
+        edge_label = edge.label
+        edges.push({ data: { source: edge.from, target: edge.to, id: e, label: edge_label, weight: edge_label } })
+    }
+
+    elements = nodes.concat(edges)
+    console.log(nodes)
+
+    var cy = cytoscape({
+        container: document.getElementById('cyto'), // container to render in
+        elements: elements,
+        style: [ // the stylesheet for the graph
+            {
+                selector: 'node',
+                style: {
+                    'height': 50,
+                    'width': 100,
+                    'background-color': '#666666',
+                    'shape': 'round-rectangle',
+                    'label': 'data(label)',
+                    'color': '#ffffff',
+                    'text-wrap': 'wrap',
+                    'text-halign': 'center',
+                    'text-valign': 'center'
+                }
+            },
+
+            {
+                selector: 'edge',
+                style: {
+                    'label': 'data(label)',
+                    'width': 1,
+                    'line-color': 'data(color)',
+                    'target-arrow-color': 'data(color)',
+                    'target-arrow-shape': 'triangle',
+                    'curve-style': 'bezier',
+                    'text-background-opacity': 1,
+                    'text-background-color': '#ffffff',
+                    'text-wrap': 'wrap',
+                    'loop-sweep': '-45deg',
+                    'control-point-step-size': 100,
+                    'segment-distances': "50 -50 20",
+
+                }
+            }
+        ],
+
+        layout: {
+            name: 'dagre',
+            nodeSep: 25,
+            rankSep: 50,
+            edgeSep: 50,
+            rankDir: 'TB',
+            nodeDimensionsIncludeLabels: true,
+            fit: true,
+        },
+    });
+
+    let defaults = {
+        menuRadius: function (ele) { return 100; }, // the outer radius (node center to the end of the menu) in pixels. It is added to the rendered size of the node. Can either be a number or function as in the example.
+        selector: 'node', // elements matching this Cytoscape.js selector will trigger cxtmenus
+        commands: [ // an array of commands to list in the menu or a function that returns the array
+
+            { 
+                fillColor: 'rgba(0, 200, 200, 0.85)', // optional: custom background color for item
+                content: 'Space', // html/text content to be displayed in the menu
+                contentStyle: {}, // css key:value pairs to set the command's css in js if you want
+                select: function (ele) { // a function to execute when the command is selected
+                    console.log(ele.id()) // `ele` holds the reference to the active element
+                },
+                hover: function (ele) { // a function to execute when the command is hovered
+                    console.log(ele.id()) // `ele` holds the reference to the active element
+                },
+                enabled: true // whether the command is selectable
+            },
+            { 
+                fillColor: 'rgba(200, 0, 200, 0.85)', // optional: custom background color for item
+                content: 'Time', // html/text content to be displayed in the menu
+                contentStyle: {}, // css key:value pairs to set the command's css in js if you want
+                select: function (ele) { // a function to execute when the command is selected
+                    console.log(ele.id()) // `ele` holds the reference to the active element
+                },
+                hover: function (ele) { // a function to execute when the command is hovered
+                    console.log(ele.id()) // `ele` holds the reference to the active element
+                },
+                enabled: true // whether the command is selectable
+            },
+            { 
+                fillColor: 'rgba(200, 100, 100, 0.85)', // optional: custom background color for item
+                content: 'Energy', // html/text content to be displayed in the menu
+                contentStyle: {}, // css key:value pairs to set the command's css in js if you want
+                select: function (ele) { // a function to execute when the command is selected
+                    console.log(ele.id()) // `ele` holds the reference to the active element
+                },
+                hover: function (ele) { // a function to execute when the command is hovered
+                    console.log(ele.id()) // `ele` holds the reference to the active element
+                },
+                enabled: true // whether the command is selectable
+            },
+
+
+        ],
+        function(ele) { return [ /*...*/] }, // a function that returns commands or a promise of commands
+        fillColor: 'rgba(0, 0, 0, 0.85)', // the background colour of the menu
+        activeFillColor: 'rgba(1, 105, 217, 0.75)', // the colour used to indicate the selected command
+        activePadding: 20, // additional size in pixels for the active command
+        indicatorSize: 24, // the size in pixels of the pointer to the active command, will default to the node size if the node size is smaller than the indicator size, 
+        separatorWidth: 3, // the empty spacing in pixels between successive commands
+        spotlightPadding: 4, // extra spacing in pixels between the element and the spotlight
+        adaptativeNodeSpotlightRadius: true, // specify whether the spotlight radius should adapt to the node size
+        openMenuEvents: 'cxttapstart taphold', // space-separated cytoscape events that will open the menu; only `cxttapstart` and/or `taphold` work here
+        itemColor: 'white', // the colour of text in the command's content
+        itemTextShadowColor: 'transparent', // the text shadow colour of the command's content
+        zIndex: 9999, // the z-index of the ui div
+    };
+
+    let menu = cy.cxtmenu(defaults);
+}
+
 function generate_graph(data) {
     nodes = []
     for (n in data.nodes) {
@@ -161,11 +338,11 @@ function generate_graph(data) {
             {
                 selector: 'node',
                 style: {
-                    'height': 100,
+                    'height': 50,
                     'width': 100,
                     'background-color': '#666666',
+                    'shape': 'round-rectangle',
                     'label': 'data(label)',
-                    'font-size': 20,
                     'color': '#ffffff',
                     'text-wrap': 'wrap',
                     'text-halign': 'center',
@@ -181,8 +358,7 @@ function generate_graph(data) {
                     'line-color': 'data(color)',
                     'target-arrow-color': 'data(color)',
                     'target-arrow-shape': 'triangle',
-                    'curve-style': 'unbundled-bezier',
-                    'visibility': 'data(visibility)',
+                    'curve-style': 'bezier',
                     'text-background-opacity': 1,
                     'text-background-color': '#ffffff',
                     'text-wrap': 'wrap',
@@ -224,15 +400,12 @@ function generate_graph(data) {
                 LEFTDOWN Chooses the left-down candidate from the four possible candidates.
                 RIGHTDOWN Chooses the right-down candidate from the four possible candidates.
                 BALANCED Creates a balanced layout from the four possible candidates. */
-                inLayerSpacingFactor: 1.0, // Factor by which the usual spacing is multiplied to determine the in-layer spacing between objects.
+                inLayerSpacingFactor: 5.0, // Factor by which the usual spacing is multiplied to determine the in-layer spacing between objects.
                 layoutHierarchy: true, // Whether the selected layouter should consider the full hierarchy
                 linearSegmentsDeflectionDampening: 0.3, // Dampens the movement of nodes to keep the diagram from getting too large.
                 mergeEdges: false, // Edges that have no ports are merged so they touch the connected nodes at the same points.
                 mergeHierarchyCrossingEdges: true, // If hierarchical layout is active, hierarchy-crossing edges use as few hierarchical ports as possible.
                 nodeLayering: 'INTERACTIVE', // Strategy for node layering.
-                /* NETWORK_SIMPLEX This algorithm tries to minimize the length of edges. This is the most computationally intensive algorithm. The number of iterations after which it aborts if it hasn't found a result yet can be set with the Maximal Iterations option.
-                LONGEST_PATH A very simple algorithm that distributes nodes along their longest path to a sink node.
-                INTERACTIVE Distributes the nodes into layers by comparing their positions before the layout algorithm was started. The idea is that the relative horizontal order of nodes as it was before layout was applied is not changed. This of course requires valid positions for all nodes to have been set on the input graph before calling the layout algorithm. The interactive node layering algorithm uses the Interactive Reference Point option to determine which reference point of nodes are used to compare positions. */
                 nodePlacement: 'BRANDES_KOEPF', // Strategy for Node Placement
                 /* BRANDES_KOEPF Minimizes the number of edge bends at the expense of diagram size: diagrams drawn with this algorithm are usually higher than diagrams drawn with other algorithms.
                 LINEAR_SEGMENTS Computes a balanced placement.
@@ -242,7 +415,7 @@ function generate_graph(data) {
                 routeSelfLoopInside: false, // Whether a self-loop is routed around or inside its node.
                 separateConnectedComponents: true, // Whether each connected component should be processed separately
                 spacing: 20, // Overall setting for the minimal amount of space to be left between objects
-                thoroughness: 20 // How much effort should be spent to produce a nice layout..
+                thoroughness: 25 // How much effort should be spent to produce a nice layout..
             }
         }
     });
@@ -252,7 +425,7 @@ function generate_graph(data) {
         commands: [ // an array of commands to list in the menu or a function that returns the array
 
             { // example command
-                fillColor: 'rgba(200, 200, 200, 0.75)', // optional: custom background color for item
+                fillColor: 'rgba(0, 200, 200, 0.85)', // optional: custom background color for item
                 content: 'Space', // html/text content to be displayed in the menu
                 contentStyle: {}, // css key:value pairs to set the command's css in js if you want
                 select: function (ele) { // a function to execute when the command is selected
@@ -264,7 +437,7 @@ function generate_graph(data) {
                 enabled: true // whether the command is selectable
             },
             { // example command
-                fillColor: 'rgba(200, 200, 200, 0.75)', // optional: custom background color for item
+                fillColor: 'rgba(200, 0, 200, 0.85)', // optional: custom background color for item
                 content: 'Time', // html/text content to be displayed in the menu
                 contentStyle: {}, // css key:value pairs to set the command's css in js if you want
                 select: function (ele) { // a function to execute when the command is selected
@@ -276,7 +449,7 @@ function generate_graph(data) {
                 enabled: true // whether the command is selectable
             },
             { // example command
-                fillColor: 'rgba(200, 200, 200, 0.75)', // optional: custom background color for item
+                fillColor: 'rgba(200, 100, 100, 0.85)', // optional: custom background color for item
                 content: 'Energy', // html/text content to be displayed in the menu
                 contentStyle: {}, // css key:value pairs to set the command's css in js if you want
                 select: function (ele) { // a function to execute when the command is selected
@@ -286,19 +459,18 @@ function generate_graph(data) {
                     console.log(ele.id()) // `ele` holds the reference to the active element
                 },
                 enabled: true // whether the command is selectable
-            }
+            },
+
 
         ],
         function(ele) { return [ /*...*/] }, // a function that returns commands or a promise of commands
-        fillColor: 'rgba(0, 0, 0, 0.75)', // the background colour of the menu
+        fillColor: 'rgba(0, 0, 0, 0.85)', // the background colour of the menu
         activeFillColor: 'rgba(1, 105, 217, 0.75)', // the colour used to indicate the selected command
         activePadding: 20, // additional size in pixels for the active command
         indicatorSize: 24, // the size in pixels of the pointer to the active command, will default to the node size if the node size is smaller than the indicator size, 
         separatorWidth: 3, // the empty spacing in pixels between successive commands
         spotlightPadding: 4, // extra spacing in pixels between the element and the spotlight
-        adaptativeNodeSpotlightRadius: false, // specify whether the spotlight radius should adapt to the node size
-        minSpotlightRadius: 24, // the minimum radius in pixels of the spotlight (ignored for the node if adaptativeNodeSpotlightRadius is enabled but still used for the edge & background)
-        maxSpotlightRadius: 38, // the maximum radius in pixels of the spotlight (ignored for the node if adaptativeNodeSpotlightRadius is enabled but still used for the edge & background)
+        adaptativeNodeSpotlightRadius: true, // specify whether the spotlight radius should adapt to the node size
         openMenuEvents: 'cxttapstart taphold', // space-separated cytoscape events that will open the menu; only `cxttapstart` and/or `taphold` work here
         itemColor: 'white', // the colour of text in the command's content
         itemTextShadowColor: 'transparent', // the text shadow colour of the command's content
